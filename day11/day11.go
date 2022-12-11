f14ff19761826aed3814080849e5b67ceb3a74d8
chema.rodriguez <chema.rodriguez@cabify.com>
2022-12-11 19:26:18 +0100

    day 11 part 2

--- day11/day11.go
+++ day11/day11.go
@@ -14,15 +14,16 @@ import (
 var input string
 
 type day struct {
-	input   string
-	monkeys []monkey
+	input     string
+	monkeys   []monkey
+	commonMod int
 }
 
 func BuildDay11() *day {
 	d := day{
 		input: input,
 	}
-	d.parse()
+	d.parse(1)
 	return &d
 
 }
@@ -32,49 +33,55 @@ func (d *day) Run() (string, string) {
 }
 
 type monkey struct {
-	items        Dequeue[uint64]
-	operation    func(uint64) uint64
-	test         func(uint64) uint64
+	items        Dequeue[int]
+	operation    func(int) int
+	test         func(int) int
 	inspectCount int
 }
 
-func (d *day) parse() {
+func (d *day) parse(part int) {
 	monkeyStr := strings.Split(strings.TrimSpace(input), "\n\n")
 	numPattern := regexp.MustCompile(`\d+`)
 	opPattern := regexp.MustCompile(`\+|\*`)
+
+	d.commonMod = 1
+
 	parseMonkey := func(s string) monkey {
 		lines := strings.Split(s, "\n")
 		m := monkey{}
 
 		startItems := Map(Expect(strconv.Atoi), numPattern.FindAllString(lines[1], -1))
-		toInt64 := func(num int) uint64 { return uint64(num) }
-		m.items = BuildDequeue(Map(toInt64, startItems)...)
+		m.items = BuildDequeue(startItems...)
 
 		op := opPattern.FindString(lines[2])
 
-		opFun := func(n uint64) (res uint64) {
+		opFun := func(n int) (res int) {
 			intNum, err := strconv.Atoi(numPattern.FindString(lines[2]))
 			if err != nil {
-				intNum = int(n)
+				intNum = n
 			}
-			opNum := uint64(intNum)
+			opNum := intNum
 			switch op {
 			case "+":
-				res = n + uint64(opNum)
+				res = n + opNum
 			case "*":
-				res = n * uint64(opNum)
+				res = n * opNum
 			default:
 				panic(fmt.Sprintf("Invalid operation %s", op))
 			}
-			worryFinal := res / 3
-			return worryFinal
+			if part == 1 {
+				return res / 3
+			}
+
+			return res % d.commonMod
 		}
 		m.operation = opFun
 
-		testDiv := uint64(Expect(strconv.Atoi)(numPattern.FindString(lines[3])))
-		testTrue := uint64(Expect(strconv.Atoi)(numPattern.FindString(lines[4])))
-		testFalse := uint64(Expect(strconv.Atoi)(numPattern.FindString(lines[5])))
-		testFun := func(n uint64) uint64 {
+		testDiv := Expect(strconv.Atoi)(numPattern.FindString(lines[3]))
+		d.commonMod *= testDiv
+		testTrue := Expect(strconv.Atoi)(numPattern.FindString(lines[4]))
+		testFalse := Expect(strconv.Atoi)(numPattern.FindString(lines[5]))
+		testFun := func(n int) int {
 			if n%testDiv == 0 {
 				return testTrue
 			}
@@ -92,22 +99,18 @@ func (d *day) parse() {
 func (d *day) printMonkeys() {
 	for i, m := range d.monkeys {
 		fmt.Printf("Monkey %d: ", i)
-
 		fmt.Printf("(inspected %d) current items -> ", m.inspectCount)
-
-		printItem := func(item uint64) uint64 {
+		printItem := func(item int) int {
 			fmt.Printf("%d, ", item)
 			return item
 		}
-
 		m.items.ApplyToAll(printItem)
 		fmt.Println()
 	}
-
 	fmt.Println()
 }
 
-func (m *monkey) doItem() (worry, nextMonkeyIndex uint64) {
+func (m *monkey) doItem() (worry, nextMonkeyIndex int) {
 	item := m.items.PopHead()
 	worry = m.operation(item)
 	nextMonkeyIndex = m.test(worry)
@@ -115,11 +118,8 @@ func (m *monkey) doItem() (worry, nextMonkeyIndex uint64) {
 	return
 }
 
-func (d *day) Run1() string {
-
-	d.printMonkeys()
-
-	for i := 0; i < 20; i++ {
+func (d *day) runSim(iters int) string {
+	for i := 0; i < iters; i++ {
 		for mi := range d.monkeys {
 			m := &d.monkeys[mi]
 			for !m.items.Empty() {
@@ -128,36 +128,19 @@ func (d *day) Run1() string {
 			}
 		}
 	}
-
-	d.printMonkeys()
-
 	getInspectCount := func(m monkey) int { return m.inspectCount }
 	counts := Map(getInspectCount, d.monkeys)
 	sort.Sort(sort.Reverse(sort.IntSlice(counts)))
 
 	return fmt.Sprint(counts[0] * counts[1])
-	// return ""
+
+}
+
+func (d *day) Run1() string {
+	return d.runSim(20)
 }
 
 func (d *day) Run2() string {
-	d.parse()
-	d.printMonkeys()
-
-	for i := 0; i < 20; i++ {
-		for mi := range d.monkeys {
-			m := &d.monkeys[mi]
-			for !m.items.Empty() {
-				worry, nextMonkeyIndex := m.doItem()
-				d.monkeys[nextMonkeyIndex].items.PushTail(worry)
-			}
-		}
-	}
-
-	d.printMonkeys()
-
-	getInspectCount := func(m monkey) int { return m.inspectCount }
-	counts := Map(getInspectCount, d.monkeys)
-	sort.Sort(sort.Reverse(sort.IntSlice(counts)))
-
-	return fmt.Sprint(counts[0] * counts[1])
+	d.parse(2)
+	return d.runSim(10000)
 }
